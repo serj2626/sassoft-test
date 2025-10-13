b7b4e7bcb80d4945a7cb96192902cb79dcd77b79
serj2626 <serj2626@mail.ru>
2025-10-14 01:19:27 +0300

    git init

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import "./style.css";
+
+import App from "./App.vue";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+const app = createApp(App);
+app.use(createPinia());
+app.mount("#app");
